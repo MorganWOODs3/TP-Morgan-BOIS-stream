c3d98f7cbdace6f4935e6c499d04eac539ea82aa
MorganWOODs3 <92862519+MorganWOODs3@users.noreply.github.com>
2022-05-09 16:38:46 +0200

    Yop12

--- stream/film/forms.py
+++ stream/film/forms.py
@@ -5,13 +5,14 @@ from . import models
 class FilmForm(ModelForm):
     class Meta:
         model = models.Film
-        fields = ('titre', 'realisateur', 'date_parution','dure','resume')
+        fields = ('titre', 'realisateur', 'date_parution','dure','resume','cat')
         labels = {
             'titre': _('Titre'),
             'realisateur': _('Réalisateur'),
             'date_parution': _('Date de parution'),
             'dure': _('Duré du film'),
-            'remume': _('Résumé')
+            'remume': _('Résumé'),
+            'cat': _('Catégorie')
         }
 
 class SiteForm(ModelForm):
--- stream/film/models.py
+++ stream/film/models.py
@@ -1,21 +1,31 @@
 from django.db import models
 
-
+cat = [
+    ('Ac', 'Action'),
+    ('An', 'Animation'),
+    ('Av', 'Aventure'),
+    ('Ho', 'Horreur'),
+    ('Fa', 'Fantastique'),
+    ('Sf', 'Science Fiction'),
+    ('Ge', 'Guerre'),
+]
 # Create your models here.
 class Film(models.Model):
     titre = models.CharField(max_length=100)
     realisateur = models.CharField(max_length=100)
     date_parution = models.DateField(blank=True, null=True)
     dure = models.IntegerField(blank=False)
+    cat = models.CharField(max_length=30, choices=cat)
     resume = models.TextField(null=True, blank=True)
 
 
+
     def __str__(self):
-        chaine = f"Titre: {self.titre} écrit par {self.realisateur} et avec {self.dure} minutes, édité le {self.date_parution} et voici le résumer {self.resume} ."
+        chaine = f"Titre: {self.titre} écrit par {self.realisateur}, dans la catégorie{self.cat} et avec une durée de {self.dure} minutes. Édité le {self.date_parution} et voici le résumer {self.resume} ."
         return chaine
 
     def dico(self):
-        return {"titre" : self.titre, "realisateur" : self.realisateur, "date_parution" : self.date_parution, "dure" : self.dure, "resume" : self.resume}
+        return {"titre" : self.titre, "realisateur" : self.realisateur, "date_parution" : self.date_parution, "dure" : self.dure, "resume" : self.resume, "cat" : self.cat}
 
 class Site(models.Model):
         titre = models.CharField(max_length=100)
@@ -27,3 +37,4 @@ class Site(models.Model):
 
         def dico(self):
             return {"titre": self.titre, "realisateur": self.url}
+
